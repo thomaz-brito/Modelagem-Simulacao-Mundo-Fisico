86bc3fc372d056bfa6514bd7963dc4b82e237dd8
Thomaz Brito <thomaz28012007@gmail.com>
2026-02-27 13:47:59 -0300

    Update ModeloAritmetico.py

--- IntroduçãoPython/ModeloAritmetico.py
+++ IntroduçãoPython/ModeloAritmetico.py
@@ -4,20 +4,19 @@
 #N=4 [nascimentos de onças/mês] 
 #M=1[mortes de onças/mês]
 
-P=list(range(40))
+P=list(range(41))
 P[0]=15
 i=0
-while(i<39):
+while(i<40):
     P[i+1]=P[i]+4-1
     i=i+1
-
 print(P)
 
 #Em seguida, plote um gráfico do número de onças em função do tempo.
 import numpy as np
-tempo=np.arange(1,41,1)
+tempo=np.arange(0,41,1)
 import matplotlib.pyplot as plt
-plt.plot( tempo , P )
+plt.plot( tempo , P)
 plt.xlabel("Tempo")
 plt.ylabel("População")
 plt.grid()
