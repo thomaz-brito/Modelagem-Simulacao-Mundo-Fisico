96aa2c2231e37a82bbcd14df8abf15c504b3dad8
Thomaz Brito <thomaz28012007@gmail.com>
2026-03-10 22:55:55 -0300

    Delete IntroduçãoPython directory

--- IntroduçãoPython/PrimeiroLoop.py
+++
@@ -1,9 +0,0 @@
-#Comece declarando uma lista de zeros chamada lista_y com o mesmo tamanho da lista_x e escreva um loop que prencha essa lista com os valores de y=x^2. Em seguida, imprima a lista_y
-
-lista_x=np.arange(0,19,1)
-lista_y=[0]*len(lista_x)
-i=0
-while(i<len(lista_x)):
-    lista_y[i]=int(lista_x[i]**2)
-    i=i+1
-print(lista_y)
